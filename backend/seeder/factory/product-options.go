785ddb4a5510f1dfa9557839f8f113ac4b23cc80
RangSheet <ranggaa606@gmail.com>
2026-03-19 22:24:19 +0700

    add availability at product options seeder

--- backend/seeder/factory/product-options.go
+++ backend/seeder/factory/product-options.go
@@ -9,29 +9,31 @@ import (
 )
 
 type ProductOptionsSeeder struct {
-    gorm_seeder.SeederAbstract
+	gorm_seeder.SeederAbstract
 }
 
 func NewProductOptionsSeeder(cfg gorm_seeder.SeederConfiguration) ProductOptionsSeeder {
-    return ProductOptionsSeeder{gorm_seeder.NewSeederAbstract(cfg)}
+	return ProductOptionsSeeder{gorm_seeder.NewSeederAbstract(cfg)}
 }
 
 // Implement Seed method to insert data
 func (s *ProductOptionsSeeder) Seed(db *gorm.DB) error {
-    var categories []model.VariantOptions
-    for i := 0; i < s.Configuration.Rows; i++ {
-        product := model.VariantOptions{
-            Name: fmt.Sprintf("Options %d", i+1),
-			VariantId: i + 1,
+	var categories []model.VariantOptions
+	for i := 0; i < s.Configuration.Rows; i++ {
+		isAvailable := i%2 == 0
+		product := model.VariantOptions{
+			Name:        fmt.Sprintf("Options %d", i+1),
+			VariantId:   i + 1,
 			CreatedById: 1,
-        }
-        categories = append(categories, product)
-    }
-    return db.CreateInBatches(categories, s.Configuration.Rows).Error
+			IsAvailable: isAvailable,
+		}
+		categories = append(categories, product)
+	}
+	return db.CreateInBatches(categories, s.Configuration.Rows).Error
 }
 
 // Implement Clear method to remove seeded data
 func (s *ProductOptionsSeeder) Clear(db *gorm.DB) error {
-    // ... logic to delete seeded data
-    return s.SeederAbstract.Delete(db, "product_option")
+	// ... logic to delete seeded data
+	return s.SeederAbstract.Delete(db, "product_option")
 }
