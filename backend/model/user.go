91a9bb233146b1ac961538f70f23d83d98eb0f05
RangSheet <ranggaa606@gmail.com>
2026-03-19 23:27:15 +0700

    add isActive attribut at user model

--- backend/model/user.go
+++ backend/model/user.go
@@ -4,8 +4,9 @@ type User struct {
 	ID       uint `gorm:"primaryKey"`
 	Name     string
 	Password string
-	Email    string
+	Email    string `gorm:"unique"`
 	Role     string `gorm:"type:varchar(8);check:role IN ('admin','customer')"`
+	IsActive bool   `gorm:"default:false"`
 	Avatar   string
 
 	// Categories            []Category
