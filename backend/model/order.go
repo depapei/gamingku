49a88f9c35e0088e7827dc75903482064391a14f
RangSheet <ranggaa606@gmail.com>
2026-03-19 22:23:23 +0700

    add customer, address, email entity to order model

--- backend/model/order.go
+++ backend/model/order.go
@@ -9,9 +9,12 @@ import (
 
 type Order struct {
 	ID         uint           `gorm:"primaryKey"`
-	UserId     int            `json:"userId"`
-	TotalPrice float64        `json:"totalPrice"`
+	UserId     *int           `json:"userId,omitempty"`
+	Customer   string         `json:"customer"`
+	Address    string         `json:"address"`
+	Email      string         `json:"email"`
 	Status     string         `gorm:"type:varchar(20);check:status IN ('pending','processing','shipped','delivered','cancelled')" json:"status"`
+	TotalPrice float64        `json:"totalPrice"`
 	CreatedAt  time.Time      `json:"createdAt"`
 	UpdatedAt  time.Time      `json:"updatedAt"`
 	DeletedAt  gorm.DeletedAt `gorm:"index"`
@@ -21,14 +24,14 @@ type Order struct {
 }
 
 type OrderItem struct {
-	ID        uint    `gorm:"primaryKey"`
-	OrderId   int     `json:"orderId"`
-	Quantity  float64 `json:"quantity"`
-	Price     float64 `json:"price"`
-	ProductId int     `json:"productId"`
+	ID        uint          `gorm:"primaryKey"`
+	OrderId   int           `json:"orderId"`
+	Quantity  float64       `json:"quantity"`
+	Price     float64       `json:"price"`
+	ProductId int           `json:"productId"`
 	VariantId pq.Int64Array `gorm:"serializer:json" json:"variantId"`
 
-	// Order Order 
+	// Order Order
 	Product Product
 	// Variant []ProductVariants
 }
