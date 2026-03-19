18f6a86c297ff7c66ce9b62740576e98d6c92fd5
RangSheet <ranggaa606@gmail.com>
2026-03-19 22:37:21 +0700

    add password at user model

--- backend/model/user.go
+++ backend/model/user.go
@@ -1,11 +1,12 @@
 package model
 
 type User struct {
-	ID     uint `gorm:"primaryKey"`
-	Name   string
-	Email  string
-	Role   string `gorm:"type:varchar(8);check:role IN ('admin','customer')"`
-	Avatar string
+	ID       uint `gorm:"primaryKey"`
+	Name     string
+	Password string
+	Email    string
+	Role     string `gorm:"type:varchar(8);check:role IN ('admin','customer')"`
+	Avatar   string
 
 	// Categories            []Category
 	// Products              []Product
