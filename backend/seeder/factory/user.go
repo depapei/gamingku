8e3a78ea3cfb4781feb2736fdeb2c851ca128e4b
RangSheet <ranggaa606@gmail.com>
2026-03-21 07:32:28 +0700

    add password at user seeder

--- backend/seeder/factory/user.go
+++ backend/seeder/factory/user.go
@@ -9,29 +9,30 @@ import (
 )
 
 type UsersSeeder struct {
-    gorm_seeder.SeederAbstract
+	gorm_seeder.SeederAbstract
 }
 
 func NewUsersSeeder(cfg gorm_seeder.SeederConfiguration) UsersSeeder {
-    return UsersSeeder{gorm_seeder.NewSeederAbstract(cfg)}
+	return UsersSeeder{gorm_seeder.NewSeederAbstract(cfg)}
 }
 
 // Implement Seed method to insert data
 func (s *UsersSeeder) Seed(db *gorm.DB) error {
-    var users []model.User
-    for i := 0; i < s.Configuration.Rows; i++ {
-        product := model.User{
-            Name: fmt.Sprintf("Test %d", i + 1),
-            Email: fmt.Sprintf("test%d@example.com", i),
-			Role: "admin",
-        }
-        users = append(users, product)
-    }
-    return db.CreateInBatches(users, s.Configuration.Rows).Error
+	var users []model.User
+	for i := 0; i < s.Configuration.Rows; i++ {
+		product := model.User{
+			Name:     fmt.Sprintf("Test %d", i+1),
+			Email:    fmt.Sprintf("test%d@example.com", i),
+			Password: fmt.Sprint(i),
+			Role:     "admin",
+		}
+		users = append(users, product)
+	}
+	return db.CreateInBatches(users, s.Configuration.Rows).Error
 }
 
 // Implement Clear method to remove seeded data
 func (s *UsersSeeder) Clear(db *gorm.DB) error {
-    // ... logic to delete seeded data
-    return s.SeederAbstract.Delete(db, "users")
+	// ... logic to delete seeded data
+	return s.SeederAbstract.Delete(db, "users")
 }
